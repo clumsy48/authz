c75ce2703e04d82e3832e3e22b70446fcf7dda07
krishna murari <m007urray@gmail.com>
2024-07-19 21:36:36 +0530

    init commit

---
+++ src/main/java/com/github/clumsy48/auth/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.github.clumsy48.auth.repositories;
+
+import com.github.clumsy48.auth.entity.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+}
